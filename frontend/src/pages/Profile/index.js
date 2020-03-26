23d671b2b16affe35e77ce16ebbfae34c5a5af10
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-03-26 18:42:18 -0300

    Add incidents cards to profile page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -13,13 +13,62 @@ export default function Profile() {
         <img src={logo} alt="" />
         <span>Bem vinda, APAD</span>
 
-        <Link className="button" to="/indidents/new">
+        <Link className="button" to="/incidents/new">
           Cadastrar novo caso
         </Link>
         <button type="button">
           <FiPower size={18} color="#e02041" />
         </button>
       </header>
+
+      <h1>Casos cadastrados</h1>
+
+      <ul>
+        <li>
+          <strong>Caso:</strong>
+          <p>Caso teste</p>
+
+          <strong>Descrição:</strong>
+          <p>Descrição teste</p>
+
+          <strong>Valor:</strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>Caso:</strong>
+          <p>Caso teste</p>
+
+          <strong>Descrição:</strong>
+          <p>Descrição teste</p>
+
+          <strong>Valor:</strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+
+        <li>
+          <strong>Caso:</strong>
+          <p>Caso teste</p>
+
+          <strong>Descrição:</strong>
+          <p>Descrição teste</p>
+
+          <strong>Valor:</strong>
+          <p>R$ 120,00</p>
+
+          <button type="button">
+            <FiTrash2 size={20} color="#a8a8b3" />
+          </button>
+        </li>
+      </ul>
     </div>
   );
 }
