1c5bf6da8c46701f66d5f991d648082a4235f71c
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-07 23:16:14 -0300

    Style incidents page

--- mobile/src/pages/incidents/index.js
+++ mobile/src/pages/incidents/index.js
@@ -1,6 +1,23 @@
 import React from "react";
-import { View } from "react-native";
+import { Image, Text, View } from "react-native";
+
+import logo from "../../assets/logo.png";
+import styles from "./styles";
 
 export default function Incidents() {
-  return <View></View>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logo} />
+        <Text style={styles.headerText}>
+          Total de <Text style={styles.headerTextBold}>0 casos</Text>.
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.description}>
+        Escolha um dos casos abaixo e salve o dia.
+      </Text>
+    </View>
+  );
 }
