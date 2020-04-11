d4d14fe2a54d9af1ff6e425774ec2938455bed8c
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-11 16:32:23 -0300

    Add first integration test

---
+++ backend/tests/integration/ong.spec.js
@@ -0,0 +1,20 @@
+const request = require("supertest");
+const app = require("../../src/app");
+const connection = require("../../database/connection");
+
+describe("ONG", () => {
+  beforeEach(() => {
+    connection.migrate.latest();
+  });
+  it("should be able to createa new ONG", async () => {
+    const response = await request(app).post("/ongs").send({
+      name: "BemTeVi",
+      email: "bemtevi@hotmail.com",
+      whatsapp: "12998324531",
+      city: "São José dos Campos",
+      uf: "SP",
+    });
+    expect(response.body).toHaveProperty("id");
+    expect(response.body.id).toHaveLength(8);
+  });
+});
