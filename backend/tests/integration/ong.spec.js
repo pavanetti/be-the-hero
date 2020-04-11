e03671e6927702d6bbdb72bfe5b24da6f3508b3b
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2020-04-11 16:55:51 -0300

    Create integration test to sessions route

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -3,23 +3,36 @@ const app = require("../../src/app");
 const connection = require("../../database/connection");
 
 describe("ONG", () => {
-  beforeEach(() => {
-    connection.migrate.latest();
+  beforeEach(async () => {
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
   });
 
   afterAll(async () => {
     await connection.destroy();
   });
 
-  it("should be able to createa new ONG", async () => {
-    const response = await request(app).post("/ongs").send({
-      name: "BemTeVi",
-      email: "bemtevi@hotmail.com",
-      whatsapp: "12998324531",
-      city: "São José dos Campos",
-      uf: "SP",
-    });
+  it("should be able to create a new ONG", async () => {
+    const response = await createOng("BemTeVi");
     expect(response.body).toHaveProperty("id");
     expect(response.body.id).toHaveLength(8);
   });
+
+  it("should be able to authenticate", async () => {
+    const name = "BemTeVi";
+    const id = (await createOng(name)).body.id;
+    const response = await request(app).post("/sessions").send({ id });
+    expect(response.body).toHaveProperty("name");
+    expect(response.body.name).toBe(name);
+  });
 });
+
+function createOng(name) {
+  return request(app).post("/ongs").send({
+    name,
+    email: "bemtevi@hotmail.com",
+    whatsapp: "12998324531",
+    city: "São José dos Campos",
+    uf: "SP",
+  });
+}
